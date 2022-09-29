82c6d29a10674cc4dcdca15367cd9f1dee03dad8
Sanarp123 <105873022+Sanarp123@users.noreply.github.com>
2022-09-29 20:06:17 +0300

    Praeiti updatai

--- functions.php
+++ functions.php
@@ -47,12 +47,131 @@ function showMessage() {
     } 
 }
 
+function getCollumns() {
+    $klientai = readJson("klientai.json");
+    $klientas = $klientai[0];
+    $collumms = array_keys($klientas);
+
+    foreach($collumms as $collumn) {
+
+        if(isset($_GET["sortCollumn"]) && $collumn == $_GET["sortCollumn"]) {
+            echo "<option value='$collumn' selected>$collumn</option>";
+        } else {
+            echo "<option value='$collumn'>$collumn</option>";
+        }
+       
+    }
+}
+
+function getCities() {
+    $klientai = readJson("klientai.json");
+    $cities = [];
+
+    foreach ($klientai as $klientas) {
+        $cities[] = $klientas["miestas"];
+    }
+
+    $cities=array_unique($cities);
+
+    foreach($cities as $key=>$city) {
+        if(isset($_GET["miestas"]) && $city == $_GET["miestas"]) {
+            echo "<option value='$city' selected>$city</option>";
+        } else {
+            echo "<option value='$city'>$city</option>";
+        }
+    }
+
+}
+//jinai gauna kaip parametra nerikiuota masyva
+//ir grazina rikiuota masyva
+function sortClients($klientai) {
+    if(isset($_GET["sortCollumn"]) && isset($_GET["sortOrder"])) {
+        $sortCollumn = $_GET["sortCollumn"];
+        $sortOrder = $_GET["sortOrder"];
+        if($sortCollumn == "id") {
+        //ASC ir DESC
+            if($sortOrder == "ASC") {
+                ksort($klientai);
+            } else if($sortOrder == "DESC") {
+                krsort($klientai);
+            }
+            //uasort funkcija
+            // teksto rikiavimas
+
+
+        } else {
+
+            $order = [-1, 1]; //ASC
+        
+            if ($sortOrder == "DESC") {
+                $order = [1, -1]; //DESC
+            }
+
+            uasort($klientai, function($dabartinis, $busimas) use($sortCollumn, $order) {    
+                //$sordOrder = ASC    -1 1
+                //$sortOrder = DESC   1 -1
+        
+               // $order = [-1, 1]; //ASC
+        
+                //if ($sortOrder == "DESC") {
+                //    $order = [1, -1]; //DESC
+                //}
+                
+                if($dabartinis[$sortCollumn] == $busimas[$sortCollumn]) {
+                    return 0;
+                } else if($dabartinis[$sortCollumn] < $busimas[$sortCollumn]) {
+                    return $order[0];
+                } else {
+                    return $order[1];
+                }
+            });
+        }        
+    } else {
+        //pagal id mazejimo tvarka
+        krsort($klientai);
+    }
+
+    if(isset($_GET["sortOrder"]) && $_GET["sortOrder"] == "RAND") {
+        
+        //nesumaiso masyvo indeksu
+        //kad sumaisytu su indeksais
+        shuffle($klientai);
+    }
+
+
+
+    return $klientai;
+}
+//jjinai gauna kaip parametra nefiltruota masyva
+//ir grazina filtruota masyva
+function filterClients($klientai) {
+
+    $miestas = "visi";
+
+    if(isset($_GET["miestas"])) {
+        $miestas = $_GET["miestas"];
+    }
+
+    $klientai=array_filter($klientai,function($klientas) use($miestas) {
+        if($miestas == "visi") {
+            return true;
+        } else if ($klientas["miestas"] == $miestas) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    //filtruojame duomenis pagal miesta
+    return $klientai;
+}
+
+
+function pagination() {}
 //void tuscia
 function getClients() {
     $klientai = readJson("klientai.json");
-    
-    krsort($klientai);
-
+    $klientai = sortClients($klientai);
+    //$klientai = filterClients($klientai);
 
     foreach($klientai as  $i => $klientas) {
         echo "<tr>";
---
+++ modal.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<div class="modal" tabindex="-1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+        
+    <title>Document</title>
+</head>
+
+  
+  <!-- Modal -->
+  <div id="simpleModal" class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Customer Details Form</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                I love ASPSnippets!
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
